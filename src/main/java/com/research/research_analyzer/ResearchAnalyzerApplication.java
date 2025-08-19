31dd3d039896ae70e3d3b6384c7ccdf8667f8db6
VedPeesu <vedkrp@gmail.com>
2025-08-19 12:35:05 -0700

    Initial commit

---
+++ src/main/java/com/research/research_analyzer/ResearchAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.research.research_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/research/research_analyzer/ResearchAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.research_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
